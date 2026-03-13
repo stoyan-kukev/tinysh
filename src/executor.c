eef6323d7a50c5bb3742783b93ab2a939ca8b485
stoyan-kukev <stoykukev@proton.me>
2026-03-13 16:18:44 +0200

    add basic output redirecting

--- src/executor.c
+++ src/executor.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <stdbool.h>
+#include <fcntl.h>
 
 void executor_init(Executor* self, char** tokens, char* path) {
   self->tokens = tokens;
@@ -56,6 +58,29 @@ int executor_run(Executor* self) {
 
     exit(0);
   }
+
+  char redirect_file_name[255] = {0};
+  bool is_overwriting = false;
+
+  int i = 0;
+  while (self->tokens[i] != NULL) {
+    const bool has_overwrite = strcmp(self->tokens[i], ">") == 0;
+    const bool has_append = strcmp(self->tokens[i], ">>") == 0;
+    if (has_overwrite || has_append) {
+      if (self->tokens[i+1] == NULL) {
+        printf("ERROR: Bad piping. Missing file to pipe to.\n");
+        return 0;
+      }
+
+      strncpy(redirect_file_name, self->tokens[i + 1], 255);
+
+      if (has_overwrite) is_overwriting = true;
+
+      self->tokens[i] = NULL;
+    }
+
+    i += 1;
+  }
   
   const pid_t id = fork();
   if (id == -1) {
@@ -64,11 +89,29 @@ int executor_run(Executor* self) {
   }
 
   if (id == 0) {
+    int fd;
+    if (redirect_file_name[0] != 0) {
+      if (is_overwriting) {
+        fd = open(redirect_file_name, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+      } else {
+        fd = open(redirect_file_name, O_WRONLY | O_CREAT | O_APPEND, 0664);
+      }
+
+      if (fd == -1) {
+        printf("ERROR: Failed to open file %s\n%s\n", redirect_file_name, strerror(errno));
+        exit(-1);
+      }
+
+      dup2(fd, STDOUT_FILENO);
+    }
+    
     int status = execvp(self->tokens[0], &self->tokens[0]);
     if (status != 0) {
       printf("ERROR: Failed to execute command\n%s\n", strerror(errno));
       exit(-1);
     }
+
+    if (redirect_file_name[0] != 0) close(fd);
   } else {
     int ret_code;
     waitpid(id, &ret_code, 0);
